77491964ac2926e14050054cbee2ee9872ae1c61
Robert Zalog <rzalog@gmail.com>
2018-10-09 08:24:43 -0700

    Remove git stuff from example-code. Change IR.

--- example-code/README.md
+++ example-code/README.md
@@ -1,2 +1,1 @@
-# Micromouse code snippets
-Helpful code snippets for micromouse. This assumes you're using mbed. If not, you probably do not need it!
+Helpful code snippets. This assumes you're using mbed. If not, you probably do not need it!
--- example-code/ir/read_ir.cpp
+++ example-code/ir/read_ir.cpp
@@ -2,8 +2,9 @@ float flash_ir(DigitalOut ir, AnalogIn rec) volatile {
     float init = rec.read();
 
     ir = 1;
+    // "US" is microseconds
     wait_us(WARM_UP_US);
-    f
+
     int n = 10;
     float total = 0;
        
@@ -14,5 +15,5 @@ float flash_ir(DigitalOut ir, AnalogIn rec) volatile {
     ir = 0;
     wait_us(COOL_DOWN_US);
     
-    return total / n - init;
+    return (total / n) - init;
 }
